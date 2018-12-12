62e47cafb4783143668b6b0ec604d65c47f47ac2
Jan Rosum <rosum@synyx.de>
2018-12-12 15:19:32 +0100

    Moved to Python stl2pov

--- src/Render.py
+++ src/Render.py
@@ -1,16 +1,55 @@
 class Render:
 
-    def __init__(self, system, random):
+    def __init__(self, system, random, stl, stl_utils):
         self.system = system
         self.random = random
+        self.stl_utils = stl_utils
+        self.stl = stl
+
         print("render class loaded")
+
+    def mesh1(self, vertices):
+        """
+        Create a POV-ray mesh description from vertices data.
+        Arguments:
+            name: The name of the object.
+            vertices: An (N,3) numpy array containing the vertex data.
+        Returns:
+            A string representation of a POV-ray mesh object.
+        """
+        facets = vertices.reshape((-1, 9))
+        lines = ["mesh {"]
+        # The indices sequence 1, 0, 2 is used because of the difference between
+        # the STL coordinate system and that used in POV-ray.
+        fct = "  triangle {{\n    <{1}, {0}, {2}>,\n    <{4}, {3}, {5}>,\n" \
+              "    <{7}, {6}, {8}>\n  }}"
+        lines += [fct.format(*f) for f in facets]
+        lines += ["texture {"]
+        lines += ["pigment { color rgb<0.9, 0.9, 0.9> }"]
+        lines += ["finish { ambient 0.2 diffuse 0.7 }"]
+        lines += ['}']
+        lines += ['}']
+        return '\n'.join(lines)
+
+    def __stl_to_pov(self , filename, pov_file):
+        vertices, name = self.stl.readstl(filename, "utf-8")
+        file = open(pov_file, 'w+')
+        file.write(self.mesh1(vertices))
+
+        global_setting_file = open("./global_povray_setting.txt", "r")
+        file.write(global_setting_file.read())
+        file.close()
+        global_setting_file.close()
+
+
+
 
     def render_stl(self, filename):
 
         pov_file = str(self.random()) + ".pov"
         png_name = "preview_" + str(self.random()) + ".png"
-        render_command = "stl2pov " + str(filename) + " > " + pov_file + " && " \
-                         "povray  -i\"" + pov_file + "\" +FN +W1920 +H1080 -o\"" + png_name + "\" +Q9 +AM1 +A +UA"
+        self.__stl_to_pov(filename, pov_file)
+        render_command = "povray  -i\"" + pov_file + "\" +FN +W1920 +H1080 -o\"" + png_name + "\" +Q9 +AM1 +A +UA"
         self.system(render_command)
         return png_name
 
