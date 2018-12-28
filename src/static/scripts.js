0bbdc151d1deb7011c819da1079201b1943c4723
Jan Rosum <rosum@synyx.de>
2018-12-28 16:32:51 +0100

    WIP: fetch for post by fileselection

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
   document.getElementById('stlfile').onchange = function () {
+
+    var formData = new FormData();
+    var fileField = document.querySelector("input[type='file']");
+
+    formData.append('stlfile', fileField.files[0]);
+
+    fetch('/uploader', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => console.log(response))
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', JSON.stringify(response)));
+
     document.getElementById('uploadtext').textContent = this.files.length + " STL ausgewählt";
   };
 });
