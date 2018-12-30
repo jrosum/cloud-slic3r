f3953c97d75f5ef57acaa0c1cd42ab8d95b2c8b0
Jan Rosum <rosum@synyx.de>
2018-12-30 19:17:37 +0100

    Refactored Frontend to upload form on file selected

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,19 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
   document.getElementById('stlfile').onchange = function () {
-
-    var formData = new FormData();
-    var fileField = document.querySelector("input[type='file']");
-
-    formData.append('stlfile', fileField.files[0]);
-
-    fetch('/uploader', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => console.log(response))
-    .catch(error => console.error('Error:', error))
-    .then(response => console.log('Success:', JSON.stringify(response)));
-
-    document.getElementById('uploadtext').textContent = this.files.length + " STL ausgewählt";
+    document.getElementById('uploadtext').textContent = "Vorschau wird berechnet";
+    document.getElementById("slicerForm").submit();
   };
 });
