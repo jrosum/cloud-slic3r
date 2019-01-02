ac64c8500781cba0e81d809c7267bf41e48f1ccc
Jan Rosum <rosum@synyx.de>
2019-01-02 14:37:06 +0100

    WIP

--- src/SendToPrinter.py
+++ src/SendToPrinter.py
@@ -6,8 +6,7 @@ class SendToPrinter:
         self.api_key = api_key
         self.system = system
 
-    def send_to_printer(self, folder_id, filename):
-        path_to_file = "temp/{}/{}".format(folder_id, filename)
+    def upload(self, path_to_file):
         curl_command = "curl -i -X POST -H \"Content-Type: multipart/form-data\" -H \"x-api-key: " + self.api_key + "\" -F \"a=upload\" -F \"filename=@[" + path_to_file + "]\" \"http://" + self.host + ":3344/printer/model/" + self.printer_name + "\""
         print(curl_command)
         return self.system(curl_command)
